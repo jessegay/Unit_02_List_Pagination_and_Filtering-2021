d183d0dad2e7ac46cd868630dacfcd4d132c9b24
Jesse Gay <jessegay@users.noreply.github.com>
2021-05-27 17:11:20 -0700

    getting started

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*
 Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
+FSJS Project 2 - Data Pagination and Filtering (Refresh 2021)
+by Jesse Gay
 */
 
 
@@ -11,14 +12,29 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+// Global variables.??
 
 
+const itemsPerPage = 10;
+list = data; // Is this it? doesn't feel right 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-
+const showPage = (list, page) => {
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   let studentList = document.querySelector('.student-list'); 
+   studentList.innerHTML = '';
+   for (let i = 0; i < list.length; i ++) {
+      if (i >= startIndex && i < endIndex) {
+         //list[i].style.display = '';
+      } else {
+         //list.style.display = 'none';
+      }
+   }
+}
 
 /*
 Create the `addPagination` function
