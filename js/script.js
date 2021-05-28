4b1546bb6379409a5fd0f7426cc874b418fbf5c6
Jesse Gay <jessegay@users.noreply.github.com>
2021-05-28 12:54:31 -0700

    Got student info into studentInfo
    Next: Append to ul

--- js/script.js
+++ js/script.js
@@ -16,7 +16,9 @@ For assistance:
 
 
 const itemsPerPage = 10;
-const list = data; // Is this it? doesn't feel right. Why don't we just refer to data? 
+// const list = data; 
+/* Is this it? doesn't feel right. Why don't we just refer to data? Should this be global, or inside showPage()? Wait, maybe I don't 
+need to even do this. I'll just pass in data when I call the function */
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
@@ -29,8 +31,18 @@ const showPage = (list, page) => {
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i ++) {
       if (i >= startIndex && i < endIndex) {
-         //list[i].style.display = '';
-         console.log(list[i].name.first);
+         //console.log(list[i].name.first);
+         let studentInfo = `<li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+           <h3>${list[i].name.first} ${list[i].name.last}</h3>
+           <span class="email">${list[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${list[i].registered.date}</span>
+         </div>
+       </li>`
+       console.log(studentInfo);
       } else {
          //list.style.display = 'none';
       }
@@ -45,4 +57,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
-showPage(list,2);
+showPage(data,2);
